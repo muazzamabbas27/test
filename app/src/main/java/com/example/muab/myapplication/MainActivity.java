423c188fde10d72735ddf24256627bc3bc035746
muazzamabbas27 <regmine3@gmail.com>
2017-12-09 13:55:13 +0500

    comment

--- app/src/main/java/com/example/muab/myapplication/MainActivity.java
+++ app/src/main/java/com/example/muab/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     //comment
     //comment
+    //comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
